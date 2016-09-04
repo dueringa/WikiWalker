a70ce78b4b01fa1fa85a22120befccca52221126
Andreas Düring <github@duering-andreas.de>
2016-09-04 15:00:52 +0200

    Add tests

---
+++ test/CollectionTests.cpp
@@ -0,0 +1,18 @@
+#include <UnitTest++/UnitTest++.h>
+
+#include "../src/ArticleCollection.h"
+
+SUITE(CollectionTests)
+{
+    TEST(MakeSureNoDuplicateArticlesExist)
+    {
+        ArticleCollection w;
+        w.add(new Article("King"));
+        w.add(new Article("Queen"));
+        CHECK_EQUAL(2, w.getNumArticles());
+        w.add(new Article("Prince"));
+        CHECK_EQUAL(3, w.getNumArticles());
+        w.add(new Article("Queen"));
+        CHECK_EQUAL(3, w.getNumArticles());
+    }
+}
