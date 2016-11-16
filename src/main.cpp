05a0fa3241f7cc7eb9d30bc509fa65a891afb232
Andreas Düring <github@duering-andreas.de>
2016-11-16 17:02:52 +0100

    Add version display

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@ int main(int argc, char** argv)
         return -1;
     }
 
+    if(std::string("-v") == argv[1] || std::string("--version") == argv[1]) {
+        std::cout << "WikiWalker, version " << _WW_VERSION << std::endl;
+        return 0;
+    }
+
     string url = argv[1];
 
     try {
@@ -29,5 +34,6 @@ void usage(string exename)
 {
     cout << "Usage: " << endl;
     cout << exename << " [URL]" << endl;
+    cout << exename << " -v" << endl;
 }
 
