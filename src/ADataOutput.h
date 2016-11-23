674fd6bd5c56d6f0d036a7dfec4816d2ca1599b2
Andreas Düring <github@duering-andreas.de>
2016-11-23 20:13:09 +0100

    Update doxygen documentation

--- src/ADataOutput.h
+++ src/ADataOutput.h
@@ -4,11 +4,24 @@
 #include <ostream>
 #include "ArticleCollection.h"
 
+/*! Abstract base class for output modules.
+ * Child classes are responsible for converting the article structure
+ * to other formats.
+ */
 class ADataOutput
 {
 public:
-    virtual void output(const Article*, std::ostream&) = 0;
-    virtual void output(const ArticleCollection&, std::ostream&) = 0;
+    /*! Output an article to an outstream.
+     * \param a pointer to article to be output
+     * \param os out stream to putput to
+     */
+    virtual void output(const Article* a, std::ostream& os) = 0;
+
+    /*! Output an article collection to an outstream.
+     * \param ac reference to article collection to be output
+     * \param os out stream to putput to
+     */
+    virtual void output(const ArticleCollection& ac, std::ostream& os) = 0;
 
     virtual ~ADataOutput() {}
 };
