af51f3a36a95ac2d83be73acc9644d17c3a8040a
Andreas Düring <github@duering-andreas.de>
2016-11-26 15:16:07 +0100

    Add comments in ADataOutput

--- src/ADataOutput.h
+++ src/ADataOutput.h
@@ -15,13 +15,15 @@ class ADataOutput
 public:
     /*! Output an article to an outstream.
      * \param a pointer to article to be output
-     * \param os out stream to putput to
+     * \param os out stream to putput to.
+     * YOU are responsible for opening and closing the stream
      */
     virtual void output(const Article* a, std::ostream& os) = 0;
 
     /*! Output an article collection to an outstream.
      * \param ac reference to article collection to be output
      * \param os out stream to putput to
+     * YOU are responsible for opening and closing the stream
      */
     virtual void output(const ArticleCollection& ac, std::ostream& os) = 0;
 
