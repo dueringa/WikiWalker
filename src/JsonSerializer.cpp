eb0ac81a07a64be99ab97a5802ec2169c7b06017
Andreas Düring <github@duering-andreas.de>
2018-03-16 18:06:44 +0100

    Remove ToJsonWriter completely

--- src/JsonSerializer.cpp
+++ src/JsonSerializer.cpp
@@ -1,14 +1,90 @@
 //! \file
 
+#include <json/json.h>
+
+#include "Article.h"
 #include "JsonSerializer.h"
-#include "ToJsonWriter.h"
 
 namespace WikiWalker
 {
+  /*! Get article links in an array.
+   * Basically undoing the Wikipedia to article conversion...
+   * \param article pointer to article which links should be extracted
+   * \return Json::Value array with titles as string
+   * \todo should / could be a member function, but then I'd have to expose
+   * Json::Value, which is ugly and clutters up other classes...
+   */
+  static Json::Value getArticleLinks(const Article* article)
+  {
+    Json::Value array(Json::ValueType::arrayValue);
+
+    for(auto ali = article->linkBegin(); ali != article->linkEnd(); ali++) {
+      auto a = ali->lock();
+      if(a != nullptr) {
+        std::string tit = a->title();
+        array.append(Json::Value(tit));
+      }
+    }
+
+    return array;
+  }
+
+  /*! Convert article to string representation of JSON representation.
+   * \param a pointer to article to be converted
+   * \return json as string
+   * \internal uses the following format:
+   * {"title":{"forward_links":[...]}}
+   */
+  static std::string convertToJson(const ArticleCollection& ac)
+  {
+    Json::Value val(Json::ValueType::objectValue);
+
+    for(auto ar : ac) {
+      Json::Value linkObj(Json::ValueType::objectValue);
+
+      if(ar.second->analyzed()) {
+        linkObj["forward_links"] = getArticleLinks(ar.second.get());
+      } else {
+        linkObj["forward_links"] = Json::Value::nullSingleton();
+      }
+
+      val[ar.first] = linkObj;
+    }
+
+    Json::StreamWriterBuilder swb;
+    swb["indentation"] = "";
+    return Json::writeString(swb, val);
+  }
+
+  /*! Convert article collection to string representation of JSON
+   * representation.
+   * \param ac reference to article collection to be converted
+   * \return json as string
+   * \internal uses the following format:
+   * {"title":{"forward_links":[...]}, "title2":{"forward_links":[...]}, ...}
+   */
+  static std::string convertToJson(const Article* a)
+  {
+    Json::Value val(Json::ValueType::objectValue);
+
+    Json::Value linkObj(Json::ValueType::objectValue);
+
+    if(a->analyzed()) {
+      linkObj["forward_links"] = getArticleLinks(a);
+    } else {
+      linkObj["forward_links"] = Json::Value::nullSingleton();
+    }
+
+    val[a->title()] = linkObj;
+
+    Json::StreamWriterBuilder swb;
+    swb["indentation"] = "";
+    return Json::writeString(swb, val);
+  }
+
   void JsonSerializer::serialize(const ArticleCollection& collection,
                                  std::ostream& outstream)
   {
-    ToJsonWriter tjw;
-    tjw.output(collection, outstream);
+    outstream << convertToJson(collection);
   }
 }
