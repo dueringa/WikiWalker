fbd554d5f84bde192684e83994b5abc7bce38d72
Andreas Düring <github@duering-andreas.de>
2018-03-16 18:06:51 +0100

    Add doxygen comments

--- inc/JsonSerializer.h
+++ inc/JsonSerializer.h
@@ -9,9 +9,16 @@
 
 namespace WikiWalker
 {
+  /*! Serialize AricleCollection from and to a custom JSON format
+   */
   class JsonSerializer
   {
   public:
+    /*! Serialize ArticleCollection to JSON in an output stream
+     * \param a pointer to article to be output
+     * \param os out stream to putput to.
+     * YOU are responsible for opening and closing the stream
+     */
     void serialize(const ArticleCollection& collection,
                    std::ostream& outstream);
   };
