6d18b95edc7726a05304a4de4c58df62f96edad3
Andreas Düring <github@duering-andreas.de>
2018-03-19 11:36:05 +0100

    Add header test to deserializer tests

--- test/JsonDeserializerTests.cpp
+++ test/JsonDeserializerTests.cpp
@@ -6,13 +6,22 @@
 #include "Article.h"
 #include "JsonSerializer.h"
 
+#include "SerializerTestDefines.h"
+
+#define WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE \
+  "{" WW_TEST_JSONSERIALIZER_PROTOCOL_HEADER_1   \
+  "," WW_TEST_JSONSERIALIZER_PROTOCOL_HEADER_2   \
+  "," WW_TEST_JSONSERIALIZER_PROTOCOL_COLLECTION_KEY
+
 SUITE(JsonDeserializerTests)
 {
   using namespace WikiWalker;
 
   TEST(Deserialize_ArticleCollection_OneArticleWithoutLinks_Unanalyzed)
   {
-    std::string jsonString = R"({"Farm":{"forward_links":null}})";
+    std::string jsonString = WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE
+        R"({"Farm":{"forward_links":null}})"
+        "}";
     std::istringstream json(jsonString);
 
     ArticleCollection ac;
@@ -28,7 +37,9 @@ SUITE(JsonDeserializerTests)
 
   TEST(Deserialize_ArticleCollection_OneArticleWithoutLinks_Analyzed)
   {
-    std::string jsonString = R"({"Farm":{"forward_links":[]}})";
+    std::string jsonString = WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE
+        R"({"Farm":{"forward_links":[]}})"
+        "}";
     std::istringstream json(jsonString);
 
     ArticleCollection ac;
@@ -46,7 +57,9 @@ SUITE(JsonDeserializerTests)
 
   TEST(Deserialize_ArticleCollection_OneArticleWithOneLink)
   {
-    std::string jsonString = R"({"Farm":{"forward_links":["Animal"]}})";
+    std::string jsonString = WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE
+        R"({"Farm":{"forward_links":["Animal"]}})"
+        "}";
     std::istringstream json(jsonString);
 
     ArticleCollection ac;
@@ -67,8 +80,9 @@ SUITE(JsonDeserializerTests)
 
   TEST(Deserialize_ArticleCollection_OneArticleWithMultipleLinks)
   {
-    std::string jsonString =
-        R"({"Farm":{"forward_links":["Animal","Pig","Equality"]}})";
+    std::string jsonString = WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE
+        R"({"Farm":{"forward_links":["Animal","Pig","Equality"]}})"
+        "}";
     std::istringstream json(jsonString);
 
     ArticleCollection ac;
@@ -97,13 +111,14 @@ SUITE(JsonDeserializerTests)
 
   TEST(Deserialize_ArticleCollection_MultipleArticlesWithMultipleLinks)
   {
-    std::string jsonString =
+    std::string jsonString = WW_TEST_JSONSERIALIZER_PROTOCOL_COMPLETE
         R"({)"
         R"("Farm":)"
         R"({"forward_links":["Animal","Pig","Equality"]},)"
         R"("Animal":)"
         R"({"forward_links":["Cat","Pig","Dog"]})"
-        R"(})";
+        R"(})"
+        "}";
     std::istringstream json(jsonString);
 
     ArticleCollection ac;
