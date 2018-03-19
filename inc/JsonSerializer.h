7197b279a3e1aee8581f52654de8ce9d1159d876
Andreas Düring <github@duering-andreas.de>
2018-03-19 09:40:21 +0100

    Specify behavior of deserializer

--- inc/JsonSerializer.h
+++ inc/JsonSerializer.h
@@ -27,13 +27,29 @@ namespace WikiWalker
 
     /*! Deserialize JSON data to an ArticleCollection
      * \param instream the stream containing the JSON data
-     * \returns The desirialized article collection
+     * \returns The desirialized article collection.
+     * \sa deserialize(ArticleCollection&, std::istream&)
      */
     ArticleCollection deserialize(std::istream& instream);
 
     /*! Deserialize JSON data to an ArticleCollection
      * \param[out] collection The desirialized article collection
      * \param instream the stream containing the JSON data
+     * \details The deserialized object / collection might not exactly match the
+     * serialized one! If a link of an article which is inside the article
+     * collection doesn't exist in the collection on serialization, it will be
+     * added to the collection on deserialization.
+     *
+     * Example: "A" is in the collection and links to "B", but "B" is not in the
+     * collection itself, so `["A" -> [ "B" ]]`. On deserialization, however,
+     * "B" is added to the collection, so the collection looks like `["A" -> [
+     * "B" ], "B" -> null]`. This is, the "new" article B is added and marked as
+     * non-analyzed.
+     *
+     * This is because of the implementation of the links as #std::weak_ptr - on
+     * serialization, they could still be in scope, but on deserialization, I
+     * need some #std::shared_ptr so the object doesn't disappear right away.
+     * This is why I deserialize them as new object in the collection.
      */
     void deserialize(ArticleCollection& collection, std::istream& instream);
   };
