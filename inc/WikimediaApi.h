63793745d97afba7945b848eb851e2d5b85c0bd0
Andreas Düring <github@duering-andreas.de>
2018-03-25 19:16:07 +0200

    Add scaffold for WikimediaApi class

---
+++ inc/WikimediaApi.h
@@ -0,0 +1,15 @@
+//! \file
+
+#ifndef WIKIWALKER_WIKIMEDIAAPI_H
+#define WIKIWALKER_WIKIMEDIAAPI_H
+
+namespace WikiWalker
+{
+  class WikimediaApi
+  {
+  public:
+  private:
+  };
+}
+
+#endif  // WIKIWALKER_WIKIMEDIAAPI_H
---
+++ src/WikimediaApi.cpp
@@ -0,0 +1,7 @@
+//! \file
+
+#include "WikimediaApi.h"
+
+namespace WikiWalker
+{
+}
